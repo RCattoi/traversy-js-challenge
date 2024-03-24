7a3a13a47ebfcc8a25ed555da9f675eee658338e
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-24 20:32:30 -0300

    Add findMaxNumber function and test cases

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,10 @@
-function findMaxNumber() {}
-
+function findMaxNumber(arr) {
+  let result = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > result) {
+      result = arr[i];
+    }
+  }
+  return result;
+}
 module.exports = findMaxNumber;
