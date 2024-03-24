752bf568ed69ce2394c5fd4a07b06751e638bbbe
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-24 20:12:54 -0300

    Add calculator function and test cases for arithmetic operations

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,22 @@
-function calculator() {}
-
+function calculator(numb1, num2, operand) {
+  let result = 0;
+  switch (operand) {
+    case '+':
+      result = numb1 + num2;
+      break;
+    case '-':
+      result = numb1 - num2;
+      break;
+    case '*':
+      result = numb1 * num2;
+      break;
+    case '/':
+      result = numb1 / num2;
+      break;
+    default:
+      throw new Error('Invalid operand');
+  }
+  return result;
+}
+console.log(calculator(1, 3, '+-'));
 module.exports = calculator;
