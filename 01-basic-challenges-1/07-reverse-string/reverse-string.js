042ef15c3553144fda9959c347c8d79c06ca9448
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-26 19:05:18 -0300

    Add reverseString function and test cases

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,15 @@
-function reverseString() {}
-
+function reverseString(word) {
+  let result = word.split('');
+  let index = 0;
+  for (var i = word.length - 1; i >= 0; i--) {
+    const temp = result[index];
+    result[index] = word[i];
+    result[i] = temp;
+    if (index + 1 === i || index === i) {
+      break;
+    }
+    index++;
+  }
+  return result.join('');
+}
 module.exports = reverseString;
