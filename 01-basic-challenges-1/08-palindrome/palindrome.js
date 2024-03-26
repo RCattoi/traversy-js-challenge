1dbcd80e7fdd15b7c422c95d2875fc63820c54c1
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-26 19:18:34 -0300

    Add palindrome test and fix isPalindrome function

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,22 @@
-function isPalindrome() {}
+function isPalindrome(word) {
+  word = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  function reverseString(word) {
+    let result = word.split('');
+    let index = 0;
+    for (var i = word.length - 1; i >= 0; i--) {
+      const temp = result[index];
+      result[index] = word[i];
+      result[i] = temp;
+      if (index + 1 === i || index === i) {
+        break;
+      }
+      index++;
+    }
+    return result.join('');
+  }
 
+  const reversed = reverseString(word);
+  console.log(reversed);
+  return word.toLowerCase() === reversed.toLowerCase();
+}
 module.exports = isPalindrome;
