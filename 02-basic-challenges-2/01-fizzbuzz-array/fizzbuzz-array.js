bdf89b827a2a965b24ebf4a6047615cb7e1a07e0
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-27 00:17:15 -0300

    Remove duplicates test and add FizzBuzz Array implementation and test

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,22 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(number) {
+  result = [];
+  for (let i = 1; i < number + 1; i++) {
+    const module3 = i % 3 === 0;
+    const module5 = i % 5 === 0;
+    const module35 = i % 3 === 0 && i % 5 === 0;
+    console.log(module3, module5, module35);
+    if (module35) {
+      result.push('FizzBuzz');
+    } else if (module3) {
+      result.push('Fizz');
+    } else if (module5) {
+      result.push('Buzz');
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
 
+console.log(fizzBuzzArray(5));
 module.exports = fizzBuzzArray;
