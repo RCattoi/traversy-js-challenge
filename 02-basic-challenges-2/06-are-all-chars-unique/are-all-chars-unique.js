6be9c3b0046b613532b5974c1a754d5c2426a40c
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-27 12:35:24 -0300

    add: function to check if all characters in a string are unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,10 @@
-function areAllCharactersUnique() {}
-
+function areAllCharactersUnique(str) {
+  const set = new Set(str);
+  return set.size === str.length;
+}
+console.log(areAllCharactersUnique('abcdefg')); // true
+console.log(areAllCharactersUnique('abcdefgA')); // true
+console.log(areAllCharactersUnique('programming')); // false
+console.log(areAllCharactersUnique('')); // true
+console.log(areAllCharactersUnique('a')); // true
 module.exports = areAllCharactersUnique;
