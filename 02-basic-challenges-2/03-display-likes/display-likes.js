a060302e7f9ebc236a73b183125a98666229f4e9
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-27 11:28:13 -0300

    Fix displayLikes function to correctly count the number of others liking

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -4,7 +4,7 @@ function displayLikes(arr) {
   if (arr.length === 2) return `${arr[0]} and ${arr[1]} like this`;
   if (arr.length === 3) return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
   if (arr.length > 3)
-    return `${arr[0]}, ${arr[1]} and ${arr.length - 1} others like this`;
+    return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
 }
 
 module.exports = displayLikes;
