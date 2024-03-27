5a26047779a78e46a824e6fe264221a7394acda4
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-27 10:08:16 -0300

    Add arrayIntersection function and test cases

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,9 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1,arr2) {
+  if(arr1.length > arr2.length){
+    return arr1 = arr1.filter((value) => arr2.includes(value));
+  } else{
+    return arr2 = arr2.filter((value) => arr1.includes(value));
+  }
+}
 
 module.exports = arrayIntersection;
