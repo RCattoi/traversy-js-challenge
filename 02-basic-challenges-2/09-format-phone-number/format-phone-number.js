a8287f17738e555b0980895bf435f51852084888
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-27 15:10:07 -0300

    add: formatPhoneNumber function and test cases

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,7 @@
-function formatPhoneNumber() {}
-
+function formatPhoneNumber(arr) {
+  return `(${arr[0]}${arr[1]}${arr[2]}) ${arr[3]}${arr[4]}${arr[5]}-${arr[6]}${arr[7]}${arr[8]}${arr[9]}`;
+}
+console.log(formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // => "(123) 456-7890"
+console.log(formatPhoneNumber([5, 1, 9, 5, 5, 5, 4, 4, 6, 8])); // => "(519) 555-4468"
+console.log(formatPhoneNumber([3, 4, 5, 5, 0, 1, 2, 5, 2, 7])); // => "(345) 501-2527"
 module.exports = formatPhoneNumber;
