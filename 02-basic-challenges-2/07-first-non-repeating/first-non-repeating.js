0065173c5192ee6a220416cabb2cca4b59a8bee0
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-27 13:18:01 -0300

    add: findFirstNonRepeatingCharacter function and test cases

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,17 @@
-function findFirstNonRepeatingCharacter() {}
-
+function findFirstNonRepeatingCharacter(string) {
+  const setString = new Set();
+  const map1 = new Map();
+  for (let i = 0; i < string.length; i++) {
+    map1.get(string[i])
+      ? map1.set(string[i], map1.get(string[i]) + 1)
+      : map1.set(string[i], 1);
+  }
+  for (let i = 0; i < map1.size; i++) {
+    if (map1.get(string[i]) === 1) return string[i];
+  }
+  return null;
+}
+console.log(findFirstNonRepeatingCharacter('aabccdeff')); // should return 'b
+console.log(findFirstNonRepeatingCharacter('aabbcc')); // should return null
+console.log(findFirstNonRepeatingCharacter('abcdef')); // should return 'a'
 module.exports = findFirstNonRepeatingCharacter;
