6f0de8f5ff450f93ba0dbf72c7c69ff714a16096
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-27 12:11:19 -0300

    refactor: findMissingLetter function and add tests

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,15 @@
-function findMissingLetter() {}
-
+function findMissingLetter(arr) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < arr.length; i++) {
+    const li = alphabet.indexOf(arr[i].toLowerCase());
+    const lip = alphabet.indexOf(arr[i + 1].toLowerCase());
+    if (lip - li > 1) {
+      if (arr[i].charCodeAt(0) >= 97) {
+        return alphabet[li + 1].toLocaleLowerCase();
+      } else {
+        return alphabet[li + 1].toLocaleUpperCase();
+      }
+    }
+  }
+}
 module.exports = findMissingLetter;
