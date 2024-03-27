46128a5d0a452fe87f9b22f56126e81ae7acf8a2
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-26 21:19:15 -0300

    Add countVowels function and test cases

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,13 @@
-function countVowels() {}
+function countVowels(word) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let count = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    if (vowels.includes(word[i].toLowerCase())) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
