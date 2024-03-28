752302d3dcfd9c14ad2028a08be8b23bf3aa2dd0
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-28 12:46:06 -0300

    feat: email validation function and tests

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,12 @@
-function validateEmail() {}
+function validateEmail(email) {
+  const regex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+  return regex.test(email);
+}
+
+console.log(validateEmail('jane.doe@domain.org'));
+console.log(validateEmail('invalid-email'));
+console.log(validateEmail('user@domain'));
+console.log(validateEmail('john@example.com'));
+console.log(validateEmail('@domain.com'));
 
 module.exports = validateEmail;
