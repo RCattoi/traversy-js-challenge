62199eb01cfe1ac9335740a56c8250456fcb0d37
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-28 23:32:24 -0300

    add calculateTotalSalesWithTax function and test cases

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,26 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(arrData, tax) {
+  const sumprice = arrData.reduce((acc, e, i) => {
+    return e.price * e.quantity + acc;
+  }, 0);
+  return sumprice + sumprice * (tax / 100);
+}
 
+calculateTotalSalesWithTax(
+  [
+    { name: 'Apple', price: 0.5, quantity: 10 },
+    { name: 'Banana', price: 0.3, quantity: 20 },
+    { name: 'Orange', price: 0.6, quantity: 15 },
+  ],
+  8
+); // 21.6 (20 + 8% tax)
+
+calculateTotalSalesWithTax(
+  [
+    { name: 'Chocolate', price: 2.5, quantity: 5 },
+    { name: 'Chips', price: 1.2, quantity: 10 },
+    { name: 'Soda', price: 1.0, quantity: 8 },
+    { name: 'Candy', price: 0.5, quantity: 15 },
+  ],
+  5
+); // 42 (40 + 5% tax)
 module.exports = calculateTotalSalesWithTax;
