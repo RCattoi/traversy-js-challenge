4601a6984984fac244e3583baf3c39539e20a57f
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-29 23:14:13 -0300

    refactor validAnagrams function and remove unused code

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,25 @@
-function validAnagrams() {}
+function validAnagrams(word1, word2) {
+  const createObj = (word) => {
+    const object = {};
+    for (let i = 0; i < word.length; i++) {
+      if (object[word[i]]) {
+        object[word[i]] += 1;
+      } else {
+        object[word[i]] = 1;
+      }
+    }
+    return object;
+  };
+
+  const mappedW1 = createObj(word1);
+  const mappedW2 = createObj(word2);
+  const equalkeys =
+    JSON.stringify(Object.keys(mappedW1).sort()) ===
+    JSON.stringify(Object.keys(mappedW2).sort());
+  const equalData =
+    JSON.stringify(Object.values(mappedW1).sort()) ===
+    JSON.stringify(Object.values(mappedW2).sort());
+  return equalkeys && equalData;
+}
 
 module.exports = validAnagrams;
