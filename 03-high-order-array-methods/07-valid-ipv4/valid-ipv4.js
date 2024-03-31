11abd1deeac6d9f6992baac5218d146f96d4489c
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-31 18:01:19 -0300

    Add validation for IPv4 addresses

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,16 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (ip) => {
+  const ipArray = ip.split('.');
+  if (ipArray.length !== 4) return false;
+  return ipArray.every((num) => {
+    if (num.length > 1 && num[0] === '0') return false;
+    return num >= 0 && num <= 255;
+  });
+};
 
+isValidIPv4('1.2.3.4'); // true
+isValidIPv4('123.45.67.89'); // true
+isValidIPv4('1.2.3'); // false
+isValidIPv4('1.2.3.4.5'); // false
+isValidIPv4('123.456.78.90'); // false
+isValidIPv4('123.045.067.089');
 module.exports = isValidIPv4;
