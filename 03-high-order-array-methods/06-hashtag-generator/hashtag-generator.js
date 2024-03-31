52d039d9738627f7aecd71812d7d51bb84ca9754
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-31 17:32:44 -0300

    Add hashtag generator function and test cases

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,20 @@
-function generateHashtag() {}
+function generateHashtag(text) {
+  if (text === '' || text.length >= 139) return false;
+  text = text.trim();
+  console.log(text);
+  const textArray = text.split(' ');
+  const mappedArray = textArray.map((word) => {
+    if (word === '') return '';
+    else return word[0].toUpperCase() + word.slice(1);
+  });
+  return `#${mappedArray.join('')}`;
+}
 
+generateHashtag('    Hello     World   ');
+// generateHashtag('JavaScript is awesome'); // "#JavaScriptIsAwesome"
+// generateHashtag('hello world'); // "#HelloWorld"
+// generateHashtag(
+//   'This is a very very very very very very very very very very very very very very long input that should result in a false hashtag because it exceeds the character limit of 140'
+// ); // false
+generateHashtag('');
 module.exports = generateHashtag;
